218c138266c3a5fbc72825608e5fb59b01334329
n8tb1t <n8tb1t@gmail.com>
2019-08-12 23:00:28 +0500

    release: beta 0.0.1

--- README.md
+++ README.md
@@ -1,1 +1,100 @@
-# catalyst-gui-wallet
+# Catalyst GUI Wallet
+
+### Features:
+This wallet contains the basic functions required to manage your Catalyst assets:
+
+* Wallet creation
+  * Create new wallet
+  * Import from private keys
+  * Import from mnemonic seed
+* Basic wallet operation
+  * Open an existing  wallet
+  * Display wallet address & balance
+  * Display private keys/seed
+  * Export private keys/seed
+  * Transactions listing/sorting/searching
+  * Display transaction detail
+  * Export incoming, outgoing, or all transactions to csv file.
+  * Incoming Transaction notification
+  * Send Catalyst to single recipient address, allow to set payment id and custom fee. Provides address lookup from addressbook.
+  * Perform wallet optimization by creating fusion transactions
+  * Provides utility to generate payment id and integrated address
+* Address book
+  * Add/Edit/Delete address entry (label/name, address and payment id)
+  * Listing/sorting/searching existing entries
+  * Allow to store same wallet address with different payment id
+  * Autosave address after sending to new/unknown recipient
+* Misc
+  * Provides setting to set local or public node address
+  * Option to use system tray (on closing/minimizing wallet)
+  * Custom node address that is not on the list will be added/remembered for future use
+  * Theme: Dark & Light Mode
+  * [Keyboard shortcuts](docs/shortcut.md)
+  * Exchange Listings Table
+
+
+### Notes
+
+The wallet relies on `catalyst-service` to manage wallet container &amp; rpc communication.
+
+On first launch, Catalyst WalletShell will try to detect location/path of bundled `catalyst-service` binary,
+but if it's failed, you can manually set path to the `catalyst-service` binary on the Settings screen.
+
+In windows configuration files are found in directory %USERPROFILE%\AppData\Roaming\catalyst-gui-wallet ,
+on issues it helps to delete it and restart the wallet.
+
+Program File are situated %USERPROFILE%\AppData\Local\Programs\catalyst-gui-wallet
+
+### Download & Run Catalyst Wallet
+
+
+#### GNU/Linux (AppImage):
+1. Download latest AppImage bundle here: https://github.com/catalystdevelopment/catalyst-gui-wallet/releases/latest
+2. Make it executable, either via GUI file manager or command line, e.g. `chmod +x catalyst-wallet-<version>-linux.AppImage`
+3. Run ./catalystd by locating the app in your Catalyst directory and double clicking, then Run the `wallet` file by double clicking the app
+4. To use own blockchain the Daemon or public node address needs setting as 127.0.0.1 and Daemon/node port set as 17291
+   To use a public node visit the main website to see a list of available nodes.
+
+
+See: https://docs.appimage.org/user-guide/run-appimages.html
+
+Build
+
+You need to have Node.js and npm installed, go to https://nodejs.org and find out how to get it installed on your platform.
+
+Once you have Node+npm installed:
+
+### first, download catalyst-service binary for each platform
+$ from Catalyst official repo
+$ https://github.com/catalystdevelopment/catalyst/releases
+$ extract the catalyst-service executable somewhere
+
+### clone the repo
+$ git clone https://github.com/catalystdevelopment/catalyst-gui-wallet.git
+$ cd catalyst-gui-wallet
+
+### install dependencies
+$ npm install
+
+### create build+dist directory
+$ mkdir -p ./build && mkdir -p ./dist
+
+### copy/symlink icons from assets, required for packaging
+$ cp ./src/assets/icon.* ./build/
+
+### build GNU/Linux package
+$ mkdir -p ./bin/lin
+$ cp /path/to/linux-version-of/catalyst-service ./bin/lin/
+$ npm run dist-lin
+
+### build Windows package
+$ mkdir -p ./bin/win
+$ cp /path/to/win-version-of/catalyst-service.exe ./bin/win/
+$ npm run dist-win
+
+### build OSX package
+$ mkdir -p ./bin/osx
+$ cp /path/to/osx-version-of/catalyst-service ./bin/osx/
+$ npm run dist-mac
+
+Resulting packages or installer can be found inside dist/ directory.
---
+++ src/js/ws_config.js
@@ -0,0 +1,100 @@
+var config = {};
+//Rebase File
+// self explanatory, your application name, descriptions, etc
+config.appName = 'catalyst-gui-wallet';
+config.appDescription = 'Catalyst GUI Wallet';
+config.appSlogan = 'Catalyst building a better tomorrow.!';
+config.appId = 'catalyst-gui-wallet';
+config.appGitRepo = 'https://github.com/catalystdevelopment/catalyst-gui-wallet';
+
+// default port number for your daemon (e.g. Catalyst daemon)
+config.daemonDefaultRpcPort = 17291;
+
+// wallet file created by this app will have this extension
+config.walletFileDefaultExt = 'nwl';
+
+// change this to match your wallet service executable filename
+config.walletServiceBinaryFilename = 'catalyst-service';
+
+// version on the bundled service (catalyst-service)
+config.walletServiceBinaryVersion = "v0.3.1.1";
+
+// config file format supported by wallet service, possible values:
+// ini -->  for turtle service (or its forks) version <= v0.8.3
+// json --> for turtle service (or its forks) version >= v0.8.4
+config.walletServiceConfigFormat = "json";
+
+// default port number for your wallet service (e.g. turtle-service)
+config.walletServiceRpcPort = 17280;
+
+// block explorer url, the [[TX_HASH]] will be substituted w/ actual transaction hash
+config.blockExplorerUrl = 'https://www.cx-explorer.gq/?hash=[[TX_HASH]]#blockchain_transaction';
+
+// default remote node to connect to, set this to a known reliable node for 'just works' user experience
+config.remoteNodeDefaultHost = 'pool.cx-pool.gq';
+
+
+// remote node list update url, set to null if you don't have one
+// for TRTL:
+// raw list: https://raw.githubusercontent.com/turtlecoin/turtlecoin-nodes-json/master/turtlecoin-nodes.json
+// filtered: https://trtl.nodes.pub/api/getNodes
+config.remoteNodeListUpdateUrl = 'https://raw.githubusercontent.com/catalystdevelopment/catalyst-gui-wallet/master/catalyst-nodes.json';
+// config.remoteNodeListUpdateUrl = null; //disables autoupdate
+
+// set to false if using raw/unfiltered node list
+config.remoteNodeListFiltered = false;
+
+// fallback remote node list, in case fetching update failed, fill this with known to works remote nodes
+config.remoteNodeListFallback = [
+  'pool.cx-pool.gq:17291',
+  '127.0.0.1:17291'
+];
+
+config.remoteNodeCacheSupported = false; //exclude unsupported nodes (bc-cache nodes)
+config.remoteNodeSslSupported = false;
+
+
+// your currency name
+config.assetName = 'Catalyst';
+// your currency ticker
+config.assetTicker = 'CX';
+// your currency address prefix, for address validation
+config.addressPrefix = 'cat';
+// standard wallet address length, for address validation
+config.addressLength = 98;
+// integrated wallet address length, for address validation. Added length is length of payment ID encoded in base58.
+config.integratedAddressLength = config.addressLength + ((64 * 11) / 8);
+
+// minimum fee for sending transaction
+config.minimumFee = 0.1;
+// minimum amount for sending transaction
+config.mininumSend = 0.1;
+// default mixin/anonimity for transaction
+config.defaultMixin = 3;
+// to represent human readable value
+config.decimalPlaces = 7;
+// to convert from atomic unit
+config.decimalDivisor = 10 ** config.decimalPlaces; //Should be 100 here
+
+// obfuscate address book entries, set to false if you want to save it in plain json file.
+// not for security because the encryption key is attached here
+config.addressBookObfuscateEntries = true;
+// key use to obfuscate address book contents
+config.addressBookObfuscationKey = '79009fb00ca1b7130832a42de45142cf6c4b7f333fe6fba5';
+// initial/sample entries to fill new address book
+config.addressBookSampleEntries = [
+  {
+    name: 'Catalyst Developers Funds',
+    address: 'cat18x93ufCQWaX4f8f2c2fZ8Ku2A4VTcgEoSpweiDbgEBF9i7YA2XGX1dSR7UbguQU5UKYEXyjfaTfnJBmRhHju26n8Y9AAFx',
+    paymentId: '',
+  }
+];
+// cipher config for private address book
+config.addressBookCipherConfig = {
+  algorithm: 'aes-256-gcm',
+  saltLenght: 128,
+  pbkdf2Rounds: 10000,
+  pbkdf2Digest: 'sha512'
+};
+
+module.exports = config;
